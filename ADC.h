64eb74e4db9f3acd6a0ac9e78e450694331050d9
sahzhang <zhangheavens@gmail.com>
2016-06-10 15:53:04 -0500

    Analog to Digital Converter

---
+++ ADC.h
@@ -0,0 +1,12 @@
+// put prototypes for public functions, explain what it does
+// put your names here, date
+
+//Initialization of ADC
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+
+void ADC_Init(void);
+
+//Sampling ADC
+uint32_t ADC_In(void);
